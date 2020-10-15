9e0d5c15ab70fa505c97415e2657101891277486
Takeda Nana <40207168+windware1203@users.noreply.github.com>
2020-10-15 14:16:47 +0800

    Update TestCourse.java

--- TestCourse.java
+++ TestCourse.java
@@ -1,4 +1,4 @@
-
+package utwork;
 import java.util.Scanner;
 /**
 Manager:Takeda Nana
@@ -13,14 +13,10 @@ public class TestCourse
 		Course course2 = new Course("Japanese","Dr. Takeda","Enhancing your Japanese language skill about reading and writing.","5473",2);
 		Course course3 = new Course("Computer Network","Dr. Ishigawa","A computer network is a group of computers that use a set of common communication protocols over digital interconnections for the purpose of sharing resources located on or provided by the network nodes. ","9511",3);
 		Course course4 = new Course("Information Security","Dr. Obama","Information security, sometimes shortened to infosec, is the practice of protecting information by mitigating information risks. It is part of information risk management.","7715",3);
-		course1.addStudent("Mamami");course1.addStudent("Nakashima");course1.addStudent("Oosaki");
-		course1.addStudent("Mashiro");course1.addStudent("Lucius");course1.addStudent("Albert");
-		course2.addStudent("Gaga");course2.addStudent("Lin s");course2.addStudent("Kei Wu");
-		course2.addStudent("Zhang osasu");course2.addStudent("ovuvuevuevue enyetuenwuevue ugbemugbem osas");
-		course3.addStudent("Iwamoto");course3.addStudent("Okamoto");course3.addStudent("Wernher ");
-		course3.addStudent("Zhang");course3.addStudent("Kim Jong-un");course3.addStudent("Gen Shi");
-		course4.addStudent("Wang");course4.addStudent("Lulu");course4.addStudent("Gate");
-		course4.addStudent("Brandt");course4.addStudent("Planck");course4.addStudent("Schweitzer");
+		course1.addStudent(109898,"LIN",1233);
+		course2.addStudent(109228,"Zoe",1233);
+		course3.addStudent(102118,"Sayuri",1021);
+		course4.addStudent(109318,"Peter",2011);
 		int usingCode=0;
 		int courseCode=0;
 		Scanner buffer = new Scanner(System.in);
